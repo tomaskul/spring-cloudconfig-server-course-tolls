8d0bf3cd1ca0d80f1f9e2255f306618e0d7621fb
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-20 16:49:57 +0100

    Add spring cloud server, using https://github.com/tomaskul/spring-cloudconfig-course-tolls repo as source

---
+++ src/test/java/io/github/tomaskul/configservergit/ConfigserverGitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tomaskul.configservergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
