9089196cf1be61f2a33dd01f18490bdcc0cd32b6
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-21 11:00:52 +0100

    Add security config bypass

---
+++ src/main/java/io/github/tomaskul/configservergit/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package io.github.tomaskul.configservergit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> {
+                            try {
+                                authz
+                                        .anyRequest().permitAll().and().csrf().disable();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+        return http.build();
+    }
+}
